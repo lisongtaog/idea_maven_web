cb9b98b16c05bd2348b46662d999ad23af387345
single <1471444606@qq.com>
2019-01-19 18:29:53 +0800

    idea maven web项目模板

---
+++ src/main/java/com/bjsxt/web/Servlet.java
@@ -0,0 +1,19 @@
+package com.bjsxt.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String username = request.getParameter("username");
+        System.out.println("用户名：" + username);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: single
+  Date: 2019-01-18
+  Time: 18:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="addUser" method="post">
+    用户名：<input type="text" name="username">
+    <input type="submit" value="OKOK">
+</form>
+</body>
+</html>
